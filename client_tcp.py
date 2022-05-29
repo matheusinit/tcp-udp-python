db0e498396a8b68cc3c061d29f38ff24ef756228
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-05-28 21:04:11 -0300

    Atualizacao com o game

--- client_tcp.py
+++ client_tcp.py
@@ -1,27 +1,62 @@
+
 #!/usr/env python3
+# JOKENPO USING SOCKET AND THE PYTHON PROGRAMMING LANGUAGE
+# AUTHORS: Felipe Rocha, Matheus Oliveira e Caio
 
-from socket import socket, AF_INET, SOCK_STREAM
+# Libraries
+import socket
+import random
 
-# Server info
-host = '127.0.0.1'
-port = 3000
+# Identification of the client's HOST AND PORT
+HOST = '127.0.0.1' # Identifies the name of the client
+PORT = 5000 # Identifies the port of the client to communicate with the server
 
-# Create TCP connection
-tcp_connection = socket(AF_INET, SOCK_STREAM)
+# List with possible moves that can be chosen by the client.
+opcoesJogadas = ['Pedra', 'Papel', 'Tesoura']
 
-# Host (IP address) and port of server to send message
-connect_to = (host, port)
+# The Socket mechanism was created to receive the connection, where in the function we pass 2 arguments, AF_INET which declares the protocol family;
+# If it was a transmission via Bluetooth, for example, it would be: AF_BLUETOOTH, and the SOCKET_STREAM, indicates that it will be TCP/IP.
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# The AF_INET constant is part of a group called address families, which are exactly the first optional parameter of the socket constructor.
+# AF_INET covers IPv4 addresses, the old Internet standard.
 
-# Make the connection
-tcp_connection.connect(connect_to)
 
-print("Sending an message...")
+# Connect to the server
+sock.connect((HOST, PORT)) # Double parentheses because connect has only one parameter.
 
-# message "Hello World" in bytes
-message = "Hello World".encode()
+while True: # The client can make n communications with the server, or until this connection is closed.
+    # The user chooses the type of move that will be made, being 1 - Random guess and 2 the user informs a guess, finally if he informs 0 (zero) the connection is closed.
+    opcao1 = int(input("\n* Escolha uma opção:\n 1- Palpite aleatório\n 2- Informar um palpite\n 0- Para encerrar.\n -> Opcao: "))
 
-# Send message
-tcp_connection.send(message)
+    # If the user chooses option 1, then the guess will be random. This randomness is given according to the Playedoptions list.
+    if (opcao1 == 1):
+        mensagemEnvioClient = random.choice(opcoesJogadas) # Randomization of the move according to the Playoptions list.
 
-# Close connection
-tcp_connection.close()
+    # If the user chooses option 2, then a menu opens to choose the guess.
+    if (opcao1 == 2):
+        opcao2 = int(input("\n* Ecolha o palpite:\n 1- Pedra\n 2- Papel\n 3- Tesoura\n -> Opcao: "))
+
+        if (opcao2 == 1):
+            mensagemEnvioClient = 'Pedra' # If the user chooses option 1, then the move will be Stone.
+        elif (opcao2 == 2):
+            mensagemEnvioClient = 'Papel' # If the user chooses option 1, then the move will be Paper.
+        elif (opcao2 == 3):
+            mensagemEnvioClient = 'Tesoura' # If the user chooses option 1, then the move will be Scissors.
+
+    # If the user informs 0 (zero) when choosing the move, then the connection is closed.
+    if (opcao1 == 0):
+        print("\nConexão encerrada!\n")
+        # Used to close the connection between the two applications.
+        sock.close()
+        break
+
+    # Used to send data to the server.
+    sock.sendall(str.encode(mensagemEnvioClient)) # Send message to server
+
+    print("\n-> Palpite enviado pelo cliente: ", mensagemEnvioClient) # Displays the guess chosen by the client
+
+    # Wait for the return from the server, a data sent by the network of up to 1024 Bytes, the 'recv' function has only 1 argument which is the size of the Buffer.
+    data = sock.recv(1024) # Bytes
+
+    # Decodes the message received by the server, capitalizes all the letters and finally this message is displayed.
+    print('\n*** Resultado final do jogo: \n', data.decode().upper())
--- server_tcp.py
+++ server_tcp.py
@@ -1,26 +1,79 @@
 #!/usr/env python3
+# JOKENPO USING SOCKET AND THE PYTHON PROGRAMMING LANGUAGE
+# AUTHORS: Felipe de Sousa Rocha, Matheus Oliveira, Caio
 
-from pydoc import cli
-from socket import SOCK_STREAM, socket, AF_INET
+import socket
+import random
 
 # Server host and port
-host = '127.0.0.1'
-port = 3000
+HOST = 'localhost' # Identifica o nome do servidor
+PORT = 5000 # Identifica a porta do servidor
+addr = (HOST, PORT)
 
-# Create TCP connection
-tcp_connection = socket(AF_INET, SOCK_STREAM)
+# List with possible moves that can be chosen by the server
+opcoesJogadas = ['Pedra', 'Papel', 'Tesoura']
 
-# Bind the scoket to address
-tcp_connection.bind((host, port))
+# The Socket mechanism was created to receive the connection, where in the function we pass 2 arguments, AF_INET which declares the protocol family;
+# If it was a transmission via Bluetooth, for example, it would be: AF_BLUETOOTH, and the SOCKET_STREAM, indicates that it will be TCP/IP.
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM TCP ID
+# The AF_INET constant is part of a group called address families, which are exactly the first optional parameter of the socket constructor.
+# AF_INET covers IPv4 addresses, the old Internet standard.
 
-# The number of connections to reveice
-tcp_connection.listen(1)
+# This line defines which IP and port the server should wait for the connection.
+sock.bind(addr)
 
-while True:
-  # Accept connection from client (and receive data from client)
-  connection, client_addr = tcp_connection.accept()
+# Sets the connection limit. And in this case, we are limiting to 5 connections.
+sock.listen(5)
 
-  print(f"Message received from {client_addr[0]}")
+print("Aguardando a conexão de um Cliente:")
 
-  # Close connection with client
-  connection.close()
+# Connection and address
+conn, ender = sock.accept() 
+
+# Presentation of the client's address, composed of the host name and the port that were connected.
+print('Connectado com', ender) 
+print("\nOs palpites do servidor são ALEATÓRIOS!\n")
+
+# The server is released indefinitely or until the connection is closed.
+    # Waits for data sent over the network of up to 1024 Bytes, the 'recv' function has only 1 argument which is the size of the Buffer.
+while True: 
+    # 1024 Byter will be received from client
+    data = conn.recv(1024) 
+
+    # print("Resposta do cliente:", data.decode())
+    
+    if not data: 
+      
+        # When there is nothing else in the data, the connection is closed
+        print("\nConexão encerrada!\n")
+
+        # Serves to close the connection between applications.
+        conn.close() 
+        break
+    
+    # Used to decode and transform the message into a string sent by the client.
+    palpiteClient = str(data.decode()) 
+    # The server guess is chosen at random, the options are inside the Playedoptions list.
+    palpiteServ = random.choice(opcoesJogadas) 
+
+    # Display server guess
+    print("* O Servidor respondeu:", palpiteServ) 
+
+    
+    # Check when the client wins the move
+    if ((palpiteClient == 'Tesoura' and palpiteServ == 'Papel') or (palpiteClient == 'Pedra' and palpiteServ == 'Tesoura') or (palpiteClient == 'Papel' and palpiteServ == 'Pedra')):
+        ganhador = 'Cliente'
+    
+    # Check when the Server wins the move
+    if ((palpiteClient == 'Papel' and palpiteServ == 'Tesoura') or (palpiteClient == 'Tesoura' and palpiteServ == 'Pedra') or (palpiteClient == 'Pedra' and palpiteServ == 'Papel')):
+        ganhador = 'Servidor'
+    
+    # Check in case of a tie
+    if (palpiteClient == palpiteServ):
+        ganhador = 'Empate'
+
+    # Return string to the client
+    result = '- Cliente: '+ str(palpiteClient) + '\n - Servidor: ' + str(palpiteServ) + '\n=> GANHADOR: ' + str(ganhador) + '\n' # Concatenation of results, being presented to the client
+
+    # Used to send the result to the client.
+    conn.sendall(bytes(str(result), 'utf8'))
